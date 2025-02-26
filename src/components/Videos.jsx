1bc86cedca7afce48bc0f833dcc782c5aefcf0e4
apps221 <maggie.a.hong@gmail.com>
2025-02-26 15:27:23 -0500

    finished and fixed search bar

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -9,7 +9,8 @@ const Videos = ({videos}) => {
     {videos.map((item, idx) => (
       <Box key={idx}>
         {item.id.videoId && <VideoCard video={item}/>}
-        {item.id.channelId && <ChannelCard channelDetail={item}/>}
+        {item.id.playlistId && <VideoCard video={item}/>}
+        {item.id.channelId && <ChannelCard channelDetail={item} />}
       </Box>
     ))}
    </Stack>
