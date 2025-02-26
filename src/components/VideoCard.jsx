8587f4c04377ba5675cec5ee6fa3f220c229b07b
apps221 <maggie.a.hong@gmail.com>
2025-02-26 12:50:18 -0500

    added channel detail

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -17,7 +17,7 @@ const VideoCard = ({video: {id: {videoId}, snippet}}) => {
         <CardContent sx={{backgroundColor: '#1e1e1e', height: '106px'}}>
             <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
             <Typography variant='subtitle1'
-            fontWeight-='bold'
+            fontWeight='bold'
             color='#FFF'>
                 {snippet?.title.slice(0, 60) || 
                 demoVideoTitle.slice(0, 60)}
